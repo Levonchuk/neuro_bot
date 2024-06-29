b948a365767dc7c3404ab802305ab005fa067ef9
Natalya Levonchuk <pamphobeteus@yandex.ru>
2024-06-29 17:26:15 +0300

    save keyboard

--- keyboard.py
+++ keyboard.py
@@ -4,10 +4,11 @@ button1 = types.KeyboardButton(text='/start')
 button2 = types.KeyboardButton(text='/ура')
 button3 = types.KeyboardButton(text='/инфо')
 button4 = types.KeyboardButton(text='/fox')
+button5 = types.KeyboardButton(text='num')
 
 keyboard1 = [
     [button1, button2, button3],
-    [button4, button1],
+    [button4, button5],
 ]
 
 
